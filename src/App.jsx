6b4613bc2a3436c6b8329e1c2d73c762fb935314
Rounak Bhowmick <rounakchakda@gmail.com>
2024-03-17 11:55:13 +0530

    init setup

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// src/App.jsx
+import React from "react";
+
+const App = () => {
+  return <div>App</div>;
+};
+
+export default App;
+
+/*
+ * HMR - Hot Module Replacement
+ * File Watcher Algorithm
+ * Bundling
+ * Minify
+ * Cleaning our code
+ * Dev and production build
+ * Super fast building algorithm
+ * Images optimization
+ * Caching while development
+ * Compression
+ * Compression with older version of browser (Pollyfills)
+ * HTTPS on dev
+ * port number
+ * Consistent hashing algorithm
+ * Zero config
+ * Transitive Dependency
+ */
